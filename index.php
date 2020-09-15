80e7a987aae74ed6298c14141fd855640b8e4cec
dimasf1994 <38943375+dimasf1994@users.noreply.github.com>
2020-09-15 21:51:08 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<link href="style.css" rel="stylesheet">
+<div class="box1">
+<p class="titik">........</p>
+<h1>
+    <span class="text1">Our</span>
+    <span class="text2">Services</span>
+</h1>
+
+<p class="our">We supply total event management down to the last detail. Our services also include event logistics, risk management, issue identification and public safety, to ensure your event runs without incident. To guarantee you are always presented in your best light, we'll also manage the venue, freight, transport, set design and production, speaker and presentation services. An Event Management provides a broad range of services thanks to ourin-house and associates talents and solutions, Whether you require event concepts, event management, exhibition and stage designs, or video production, we can provide you with your needs competitively. Our services includes:</p>
+<br><br><br><br><br><br>
+<div class="image">
+<br><br><br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<img src="img/bag.jpg" alt="avatar">
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<img src="img/lamp.jpg" alt="avatar">
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<img src="img/diagram.png" alt="avatar">
+</div>
+
+</div>
+<div class="servicelist">
+    <br><br><br><br>
+    <ul style="list-style-type:disk;">
+  <li>Creative concepts and ideas, below the line and collaterals</li>
+  <li>Staging designs, decorations, mechanism and constructions</li>
+  <li>Venue logistics - technical blueprints</li>
+  <li>Hospitality and destination management services, F&B and entertainment</li>
+  <li>Supply and installation of hardware, software and items require for the event</li>
+  <li>Pre-production - video editing, 2d & 3d animations, multimedia graphic</li>
+  <li>Designing, finish artwork and music editing</li>
+</ul>  
+</div>
